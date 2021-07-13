a46036d505e051f17e0e463516a9d96a99f7359b
Sherex mykes <sherexmykes@gmail.com>
2021-07-12 23:49:01 -0400

    Removed unused import of fb on App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import { fb } from '../../lib/firebase.js';
+import '../../lib/firebase.js';
 import '@firebase/firestore';
+
 import TestDbWrite from '../TestDbWrite/TestDbWrite';
 import TestDbRead from '../TestDbRead/TestDbRead';
+
 function App() {
   return (
-    <div>
+    <div className="App">
       <TestDbWrite />
       <TestDbRead />
     </div>

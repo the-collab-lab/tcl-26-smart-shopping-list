878c6ff0b3b1c51b39abb97b5aeed72e73f20ac0
Ander Swift <anderswift@gmail.com>
2021-07-18 00:28:08 -0400

    Update firebase config and initialization

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,17 +3,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-// Initalize Firebase.
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: 'AIzaSyDlgVTZkjq6HT0pFm7sBiSm60EQ19D2RrI',
-    authDomain: 'tcl-26-shopping-list.firebaseapp.com',
-    projectId: 'tcl-26-shopping-list',
-    storageBucket: 'tcl-26-shopping-list.appspot.com',
-    messagingSenderId: '368940430821',
-    appId: '1:368940430821:web:5347ad9d833f3a84cd6b0c',
-  });
-}
+const firebaseConfig = {
+  apiKey: 'AIzaSyDlgVTZkjq6HT0pFm7sBiSm60EQ19D2RrI',
+  authDomain: 'tcl-26-shopping-list.firebaseapp.com',
+  projectId: 'tcl-26-shopping-list',
+  storageBucket: 'tcl-26-shopping-list.appspot.com',
+  messagingSenderId: '368940430821',
+  appId: '1:368940430821:web:5347ad9d833f3a84cd6b0c',
+};
 
-const db = firebase.firestore();
+const firebaseInstance = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseInstance.firestore();
 export { db };

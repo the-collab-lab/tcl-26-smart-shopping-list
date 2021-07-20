851c6a4d0e0f072431fa0a76318f6f6c9412a844
Nick Zanetti <nzanetti44@gmail.com>
2021-07-20 19:53:41 -0400

    create new home page with button to add a new list and store a token in local storage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 // Packages
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
 // External Files
 import './App.css';
@@ -9,16 +14,28 @@ import './App.css';
 import List from './pages/List/List';
 import AddItem from './pages/AddItem/AddItem';
 import NotFound from './pages/NotFound/NotFound';
+import Home from './pages/Home';
 
 // Components
 import NavMenu from './components/NavMenu/NavMenu';
 
 function App() {
+  const [haveToken, setHaveToken] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setHaveToken(true);
+    }
+  }, []);
   return (
     <Router>
       <div className="App container">
         <Switch>
           <Route exact path="/">
+            {haveToken ? <Redirect to="/list" /> : <Home />}
+          </Route>
+          <Route path="/list">
             <List />
           </Route>
           <Route path="/add">
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import getToken from '../lib/tokens';
+
+const Home = () => {
+  const saveToken = () => {
+    const token = getToken();
+    localStorage.setItem('token', token);
+    window.location.reload(false);
+  };
+  return (
+    <main>
+      <h1>Welcome To Your Smart Shopping List</h1>
+      <button onClick={saveToken}>Create a new list</button>
+    </main>
+  );
+};
+
+export default Home;

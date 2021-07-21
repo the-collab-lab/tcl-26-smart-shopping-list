da4507e4103a365cf89f533136bfbda847cb86e4
Ander Swift <anderswift@gmail.com>
2021-07-20 21:20:00 -0400

    Make form inputs into controlled components
    Form inputs are connected on change to state variable formValues.
    
    Co-authored-by: Connie Tran <connietrandev@gmail.com>

--- src/components/AddItemForm/AddItemForm.js
+++ src/components/AddItemForm/AddItemForm.js
@@ -1,35 +1,73 @@
+import { useState } from 'react';
+
 const AddItemForm = () => {
+  const [formValues, setFormValues] = useState({
+    itemName: '',
+    purchaseInterval: '7',
+  });
+
+  // Generic function that works for any of the below form inputs
+  const handleChange = (event) => {
+    const inputName = event.target.name;
+    setFormValues({ ...formValues, [inputName]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // handle submitting form values to database here
+  };
+
   return (
     <form>
-      <label htmlFor="item-name">Item name:</label>
+      <label htmlFor="itemName">Item name:</label>
       <input
-        id="item-name"
-        name=""
         type="text"
-        aria-describedby="item-name-hint"
+        id="itemName"
+        name="itemName"
+        aria-describedby="itemNameHint"
+        value={formValues.itemName}
+        onChange={handleChange}
+        maxLength="100"
+        required
       />
-      <span id="item-name-hint"></span>
+      <span id="itemNameHint"></span>
 
       <fieldset>
         <legend>How soon will you buy this again?</legend>
 
         <input
-          id="soon-option"
-          name="how-soon"
           type="radio"
+          id="soonOption"
+          name="purchaseInterval"
           value="7"
-          checked
+          onChange={handleChange}
+          checked={formValues.purchaseInterval === '7'}
         />
-        <label htmlFor="soon-option">Soon</label>
+        <label htmlFor="soonOption">Soon</label>
 
-        <input id="kinda-soon-option" name="how-soon" type="radio" value="14" />
-        <label htmlFor="kinda-soon-option">Kind of Soon</label>
+        <input
+          type="radio"
+          id="kindaSoonOption"
+          name="purchaseInterval"
+          value="14"
+          onChange={handleChange}
+          checked={formValues.purchaseInterval === '14'}
+        />
+        <label htmlFor="kindaSoonOption">Kind of Soon</label>
 
-        <input id="not-soon-option" name="how-soon" type="radio" value="30" />
-        <label htmlFor="not-soon-option">Not Soon</label>
-
-        <button type="submit">Add Item</button>
+        <input
+          type="radio"
+          id="notSoonOption"
+          name="purchaseInterval"
+          value="30"
+          onChange={handleChange}
+          checked={formValues.purchaseInterval === '30'}
+        />
+        <label htmlFor="notSoonOption">Not Soon</label>
       </fieldset>
+
+      <button type="submit">Add Item</button>
     </form>
   );
 };

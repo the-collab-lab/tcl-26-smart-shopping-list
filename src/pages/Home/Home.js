ffbce5e3ab02ba9c3fae4c02ab04119b26299e75
Ander Swift <anderswift@gmail.com>
2021-07-27 21:50:53 -0400

    Add JSX form join list form
    Co-authored-by: Nick Zanetti <64228185+nick-zanetti@users.noreply.github.com>

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 function Home({ createList }) {
   let history = useHistory();
 
-  function handleClick() {
+  function handleCreateList() {
     createList()
       .then((success) => {
         console.log(success);
@@ -16,10 +16,42 @@ function Home({ createList }) {
 
   return (
     <main>
-      <h1>Welcome To Your Smart Shopping List</h1>
-      <button type="button" onClick={handleClick}>
-        Create a new list
-      </button>
+      <header className="container__header header">
+        <h1>Welcome To Your Smart Shopping List</h1>
+      </header>
+
+      <div className="new-list-section">
+        <button type="button" onClick={handleCreateList}>
+          Create a new list
+        </button>
+      </div>
+
+      <div className="container__separator">- or -</div>
+
+      <div className="join-list-section">
+        <form name="joinListForm" className="join-list-form">
+          <div aria-live="assertive" className="join-list__error error"></div>
+          <label
+            className="join-list-form__label join-list-form__label_type_text label"
+            htmlFor="shareToken"
+          >
+            Item name:
+          </label>
+          <input
+            className="join-list-form__text-field text-field"
+            type="text"
+            id="shareToken"
+            name="shareToken"
+            value=""
+            maxLength="100"
+            required
+          />
+
+          <button type="submit" className="join-list-form__submit button">
+            Join an existing list
+          </button>
+        </form>
+      </div>
     </main>
   );
 }

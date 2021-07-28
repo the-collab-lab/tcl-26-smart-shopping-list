a740557f8c01d3062a0b0262b514fa500ed014f7
Ander Swift <anderswift@gmail.com>
2021-07-27 22:44:44 -0400

    Fix text/markup and CSS classes in Home view
    Co-authored-by: Nick Zanetti <64228185+nick-zanetti@users.noreply.github.com>

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -32,31 +32,39 @@ function Home({ createList, joinList }) {
   }
 
   return (
-    <main>
+    <>
       <header className="container__header header">
-        <h1>Welcome To Your Smart Shopping List</h1>
+        <h1 className="header__welcome">Welcome To Your Smart Shopping List</h1>
       </header>
 
-      <div className="new-list-section">
-        <button type="button" onClick={handleCreateList}>
-          Create a new list
-        </button>
-      </div>
+      <main>
+        <div className="new-list">
+          <button
+            type="button"
+            className="new-list__button button"
+            onClick={handleCreateList}
+          >
+            Create a new list
+          </button>
+        </div>
 
-      <div className="container__separator">- or -</div>
+        <div className="container__separator">- or -</div>
 
-      <div className="join-list-section">
         <form
           name="joinListForm"
           onSubmit={handleJoinList}
           className="join-list-form"
         >
-          <div aria-live="assertive" className="join-list__error error"></div>
+          <p>Join an existing shopping list by entering a three word token.</p>
+          <div
+            aria-live="assertive"
+            className="join-list-form__error error"
+          ></div>
           <label
             className="join-list-form__label join-list-form__label_type_text label"
             htmlFor="shareToken"
           >
-            Item name:
+            Share Token:
           </label>
           <input
             className="join-list-form__text-field text-field"
@@ -73,8 +81,8 @@ function Home({ createList, joinList }) {
             Join an existing list
           </button>
         </form>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 

bb0ed22e140ef0b9dba6e0544f65ce6e9483d52c
Connie Tran <connietrandev@gmail.com>
2021-08-02 20:26:39 -0400

    Add conditional rendering when shopping list is empty
    Co-authored-by: Nick Zanetti <64228185+nick-zanetti@users.noreply.github.com>

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -1,11 +1,18 @@
 import { db } from '../../lib/firebase.js';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import ShoppingListItem from '../ShoppingListItem/ShoppingListItem.js';
+import { useHistory } from 'react-router-dom';
 
 function ShoppingList({ listId }) {
+  let history = useHistory();
+
   const [listItems, loading, error] = useCollection(
     db.collection(`lists/${listId}/items`).orderBy('purchaseInterval', 'asc'),
   );
+
+  const handleClick = () => {
+    history.push('/add');
+  };
 
   return (
     <div className="shopping-list">
@@ -21,12 +28,23 @@ function ShoppingList({ listId }) {
         </div>
       )}
 
-      {!loading && listItems && (
+      {/* !loading is required to access listItems array */}
+      {/* listItems.empty is a Boolean for whether querySnapshot is empty */}
+      {!loading && !listItems.empty && (
         <ul className="shopping-list__list">
           {listItems.docs.map((doc) => (
             <ShoppingListItem key={doc.id} item={doc.data()} />
           ))}
         </ul>
+      )}
+
+      {!loading && listItems.empty && (
+        <>
+          <p>Your shopping list is currently empty.</p>
+          <button className="button" type="button" onClick={handleClick}>
+            Add Item
+          </button>
+        </>
       )}
     </div>
   );

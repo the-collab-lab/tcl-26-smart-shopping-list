4a57f83fc9f37f4fad29f5d551262ebda2039149
Ander Swift <anderswift@gmail.com>
2021-08-09 22:11:54 -0400

    Add functionality to save purchase interval to db
    Within checkAsPurchased (formerly handleCheck), use
    calculateEstimate function from lib to get new
    purchase interval and save to firestore.
    
    Co-authored-by: Connie Tran <connietrandev@gmail.com>

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -3,6 +3,8 @@ import { db } from '../../lib/firebase.js';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import ShoppingListItem from '../ShoppingListItem/ShoppingListItem.js';
 import { useHistory } from 'react-router-dom';
+import calculateEstimate from '../../lib/estimates.js';
+import { DateTime } from 'luxon';
 
 function ShoppingList({ listId }) {
   let history = useHistory();
@@ -11,13 +13,52 @@ function ShoppingList({ listId }) {
     db.collection(`lists/${listId}/items`).orderBy('purchaseInterval', 'asc'),
   );
 
-  const handleCheck = (e) => {
-    const itemId = e.target.value;
+  /*
+   * Helper function to get the latest interval between purchases (takes Luxon date objects)
+   * Note: must account for the possibility an item hasn't been purchased, so lastPurchaseDate is null
+   **/
+  const getLatestInterval = (args) => {
+    const { lastPurchaseDate, newPurchaseDate, defaultInterval } = args;
+
+    if (lastPurchaseDate instanceof DateTime) {
+      // if lastPurchaseDate is a valid date, calculate the interval
+      const duration = newPurchaseDate.diff(lastPurchaseDate, ['days']);
+      return Math.round(duration.as('days'));
+    } else {
+      // otherwise, return the default value provided
+      return defaultInterval;
+    }
+  };
+
+  const checkAsPurchased = (itemId, item) => {
+    // convert lastPurchaseDate from firestore and JS current time to Luxon DateTime objects
+    const lastPurchaseDate = item.lastPurchaseDate?.seconds
+      ? DateTime.fromSeconds(item.lastPurchaseDate.seconds)
+      : null; // for new items, lastPurchaseDate will be null so keep it null
+    const newPurchaseDate = DateTime.fromMillis(Date.now());
+
+    /* since an interval can't be calculated when the lastPurchaseDate is null,
+     * we provide the saved purchaseInterval as a default (the user's initial estimate) */
+    const latestInterval = getLatestInterval({
+      lastPurchaseDate: lastPurchaseDate,
+      newPurchaseDate: newPurchaseDate,
+      defaultInterval: item.purchaseInterval,
+    });
+
+    const newPurchaseInterval = calculateEstimate(
+      item.purchaseInterval,
+      latestInterval,
+      item.numberOfPurchases,
+    );
+
     db.collection(`lists/${listId}/items`)
       .doc(itemId)
       .update({
-        lastPurchaseDate: firebase.firestore.FieldValue.serverTimestamp(),
+        lastPurchaseDate: firebase.firestore.Timestamp.fromMillis(
+          newPurchaseDate.toMillis(),
+        ),
         numberOfPurchases: firebase.firestore.FieldValue.increment(1),
+        purchaseInterval: newPurchaseInterval,
       })
       .catch((err) => {
         console.log(err);
@@ -47,7 +88,7 @@ function ShoppingList({ listId }) {
               listId={listId}
               itemId={doc.id}
               item={doc.data()}
-              handleCheck={handleCheck}
+              checkAsPurchased={checkAsPurchased}
             />
           ))}
         </ul>

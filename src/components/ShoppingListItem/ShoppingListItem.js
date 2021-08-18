16ef90628ffcc3f022c2099e02eddde30adcdbf2
Ander Swift <anderswift@gmail.com>
2021-08-18 19:13:20 -0400

    Add class, screen reader text to show item status
    Co-authored-by: Nick Zanetti <64228185+nick-zanetti@users.noreply.github.com>

--- src/components/ShoppingListItem/ShoppingListItem.js
+++ src/components/ShoppingListItem/ShoppingListItem.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 import isUnder24hSincePurchased from '../../utils/isUnder24hSincePurchased.js';
 
-const ShoppingListItem = ({ listId, itemId, item, checkAsPurchased }) => {
+const ShoppingListItem = ({ item, checkAsPurchased }) => {
   const [recentlyPurchased, setRecentlyPurchased] = useState(false);
 
   // update whether item is recently purchased
@@ -17,19 +17,28 @@ const ShoppingListItem = ({ listId, itemId, item, checkAsPurchased }) => {
   return (
     <li className="shopping-list__item item">
       <input
-        id={`item-${itemId}`}
-        value={itemId}
+        id={`item-${item.id}`}
+        value={item.id}
         type="checkbox"
         disabled={recentlyPurchased}
         checked={recentlyPurchased}
-        className={`checkbox shopping-list__checkbox ${recentlyPurchased} ? 'checkbox_recently-purchased' : '' `}
-        onChange={() => checkAsPurchased(itemId, item)}
+        className={`checkbox shopping-list__checkbox 
+        ${recentlyPurchased} ? 'checkbox_recently-purchased' : '' 
+        checkbox_${item.status}
+        `}
+        onChange={() => checkAsPurchased(item)}
       />
       <label
         className="label label_check-radio shopping-list__label"
-        htmlFor={`item-${itemId}`}
+        htmlFor={`item-${item.id}`}
       >
         {item.itemName}
+        <span className="visually-hidden">
+          {item.status === 'soon' && ' Need to buy soon'}
+          {item.status === 'kind-of-soon' && ' Need to buy kind of soon'}
+          {item.status === 'not-soon' && " Don't need to buy soon"}
+          {item.status === 'inactive' && ' Inactive'}
+        </span>
       </label>
     </li>
   );

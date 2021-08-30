9fc21a014f228a961c7fa78446fb916de3ad3be6
Ander Swift <anderswift@gmail.com>
2021-08-30 01:30:23 -0400

    Add mobile logo styles

--- src/components/LogoHeader/LogoHeader.js
+++ src/components/LogoHeader/LogoHeader.js
@@ -6,11 +6,20 @@ const LogoHeader = ({ isHome = false }) => {
   return (
     <header className={`logo ${isHome ? 'logo_home' : ''}`}>
       {isHome /* Make the logo the h1 on home page only */ ? (
-        <h1 className="logo__text logo__text">Peasy</h1>
+        <h1 className="logo__heading logo__heading_home">
+          Peasy
+          <img
+            className="logo__image logo__image_home"
+            src={peasyLogoMark}
+            alt="logo"
+          />
+        </h1>
       ) : (
-        <h1 className="logo__text logo__text">Peasy</h1>
+        <div className="logo__heading">
+          Peasy
+          <img className="logo__image" src={peasyLogoMark} alt="logo" />
+        </div>
       )}
-      <img className="logo__image logo__image" src={peasyLogoMark} alt="logo" />
     </header>
   );
 };

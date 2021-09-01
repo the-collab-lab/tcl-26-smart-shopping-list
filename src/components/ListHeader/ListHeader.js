d752534fc4bf899f2b1a93c93f22aaa48b9ab8c1
Connie Tran <connietrandev@gmail.com>
2021-08-31 21:26:46 -0400

    Add button link to Shopping List to show all item details

--- src/components/ListHeader/ListHeader.js
+++ src/components/ListHeader/ListHeader.js
@@ -4,6 +4,10 @@ import peasyLogoMark from '../../images/peasy-logomark.svg';
 import ShareToken from '../ShareToken/ShareToken.js';
 
 const ListHeader = ({ token }) => {
+  const showAllItemDetails = () => {
+    console.log('clicked!');
+  };
+
   return (
     <header className="list-view__header list-header">
       <div className="list-header__logo logo">
@@ -12,6 +16,10 @@ const ListHeader = ({ token }) => {
           <img className="logo__image" src={peasyLogoMark} alt="logo" />
         </h1>
       </div>
+
+      <button type="button" className="link" onClick={showAllItemDetails}>
+        Show all item details
+      </button>
 
       <ShareToken token={token} />
     </header>

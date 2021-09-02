ff08c5816c0b77665645c4215f75f60a3446e8a2
Ander Swift <anderswift@gmail.com>
2021-09-02 00:20:23 -0400

    Fix share token show/hide behaviour
    Co-authored-by: Sheila Moore <Sherexmykes@gmail.com>

--- src/components/ShareToken/ShareToken.js
+++ src/components/ShareToken/ShareToken.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import './ShareToken.css';
 
@@ -6,6 +6,7 @@ import { ReactComponent as ShareIcon } from '../../images/icon-share.svg';
 
 const ShareToken = ({ token }) => {
   const [showMobileShare, setShowMobileShare] = useState(false);
+  const [showDesktopShare, setShowDesktopShare] = useState(false);
   const shareTokenRef = useRef();
 
   function copyToken() {
@@ -16,9 +17,37 @@ const ShareToken = ({ token }) => {
 
   const handleTokenShare = () => {
     copyToken();
+
+    // toggle mobile token visibility
     setShowMobileShare(!showMobileShare);
-    shareTokenRef.current.focus();
+
+    // show token on desktop and direct focus to field
+    setShowDesktopShare(true);
   };
+
+  // when share area is shown, direct focus to field (important for screen readers to follow along)
+  useEffect(() => {
+    if (showMobileShare) shareTokenRef.current.focus();
+  }, [showMobileShare]);
+
+  /* this extra listener ensures the token is still hidden on click away if it's opened on a narrower/mobile 
+    view, then the user changes screen orientation or browser size to wider view. Otherwise it can get stuck 
+    open if the onBlur event on the text field has already happened.  */
+  useEffect(() => {
+    const handleTokenHide = (e) => {
+      if (!shareTokenRef.current.contains(e.target)) setShowDesktopShare(false);
+    };
+
+    if (showDesktopShare) {
+      // when share area is shown, direct focus to field (important for screen readers to follow along)
+      shareTokenRef.current.focus();
+      document.addEventListener('click', handleTokenHide);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleTokenHide);
+    };
+  }, [showDesktopShare]);
 
   return (
     <>
@@ -26,9 +55,10 @@ const ShareToken = ({ token }) => {
         type="button"
         onClick={handleTokenShare}
         className="button list-header__share-toggle-button share-toggle-button"
-        aria-expanded={showMobileShare}
-        aria-controls="share-token"
-        aria-label="Share list"
+        aria-label={`${
+          showDesktopShare ? 'Copied to clipboard' : 'Share list'
+        }`}
+        aria-describedby="share-toke-hint"
       >
         <ShareIcon
           aria-hidden="true"
@@ -38,33 +68,40 @@ const ShareToken = ({ token }) => {
         <strong className="share-toggle-button__strong" htmlFor="shareToken">
           Share your list
         </strong>
-        <span className="share-toggle-button__hint">
-          Reveal your token and copy to clipboard.
-        </span>
       </button>
+
+      <span
+        className={`share-token-hint list-header__share-token-hint ${
+          !showDesktopShare ? 'share-token-hint_show' : ''
+        }`}
+        id="share-toke-hint"
+      >
+        Reveal your token and copy to clipboard.
+      </span>
 
       <div
         className={`share-token list-header__share-token ${
           showMobileShare ? 'share-token_mobile_show' : ''
-        }`}
+        } ${showDesktopShare ? 'share-token_desktop_show' : ''}`}
         id="share-token"
-        role="region"
-        tabIndex="-1"
-        ref={shareTokenRef}
       >
         <label className="share-token__label" htmlFor="shareToken">
           Your unique token:
         </label>
-        <div className="form-group clipboard-copy clipboard-copy">
-          <input
-            className="share-token__token text-field form-group__text-field"
-            type="text"
-            id="shareToken"
-            name="shareToken"
-            value={token}
-            readOnly
-          />
-        </div>
+        <input
+          className="share-token__token text-field"
+          type="text"
+          id="shareToken"
+          name="shareToken"
+          value={token}
+          ref={shareTokenRef}
+          onBlur={() => setShowDesktopShare(false)}
+          aria-describedby="token-copied"
+          readOnly
+        />
+        <span className="share-token__copied" id="token-copied">
+          Copied to clipboard!
+        </span>
       </div>
     </>
   );

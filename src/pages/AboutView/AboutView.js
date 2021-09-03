de94d76af8b8f7c300aee9f41d6e61814877be68
Ander Swift <anderswift@gmail.com>
2021-09-03 18:22:49 -0400

    Wide screen styles for about page

--- src/pages/AboutView/AboutView.js
+++ src/pages/AboutView/AboutView.js
@@ -1,7 +1,9 @@
-import screenshot from './desktop-screen.png';
-import './about-view.css';
 import { NavLink } from 'react-router-dom';
-const { Helmet } = require('react-helmet');
+import { Helmet } from 'react-helmet';
+
+import LogoHeader from '../../components/LogoHeader/LogoHeader';
+import phoneSample from '../../images/phone.png';
+import './AboutView.css';
 
 const AboutView = ({ listId }) => {
   return (
@@ -9,74 +11,124 @@ const AboutView = ({ listId }) => {
       <Helmet>
         <title>About - Peasy</title>
       </Helmet>
-      <div className="about-images">
-        <img src={screenshot} alt="desktop view" className="desktop__image" />
-      </div>
+
+      <LogoHeader />
+
       <main className="page-view__main about-text">
         <h1 className="about-text__header">How Peasy Works</h1>
-        <section className="about-section">
-          <h2 className="about-section__header">
-            Add items to your grocery list
-          </h2>
-          <p className="about-section__paragraph">
-            Add items you regularly buy to your list. When you add a new item,
-            Peasy will ask you to estimate how soon you'll need to buy it again.
-          </p>
+
+        <section className="about-text__section about-section">
+          <img
+            src={phoneSample}
+            alt="Screenshot of adding items to your list"
+            className="about-section__image"
+          />
+          <div className="about-section__description">
+            <h2 className="about-section__heading">
+              Add groceries to your list
+            </h2>
+            <p className="about-section__paragraph">
+              Add items you regularly buy to your list. When you add a new item,
+              Peasy will ask you to estimate how soon you'll need to buy it
+              again.
+            </p>
+          </div>
         </section>
-        <section className="about-section">
-          <h2 className="about-section__header">Check items off your list</h2>
-          <p className="about-section__paragraph">
-            As you buy your groceries, check them off your list to help Peasy
-            learn how often you need different items.
-          </p>
+
+        <section className="about-text__section about-section">
+          <img
+            src={phoneSample}
+            alt="Screenshot of adding items to your list"
+            className="about-section__image"
+          />
+          <div className="about-section__description">
+            <h2 className="about-section__heading">
+              Check items off as you buy
+            </h2>
+            <p className="about-section__paragraph">
+              As you buy your groceries, check them off your list to help Peasy
+              learn how often you need different items.
+            </p>
+          </div>
         </section>
-        <section className="about-section">
-          <h2 className="about-section__header">
-            Peasy predicts what you'll need to buy next
-          </h2>
-          <p className="about-section__paragraph">
-            Your list gets smarter with time! Peasy uses your buying history to
-            predict what you'll need to buy next and sort your list
-            automatically.
-          </p>
-          <p className="about-section__paragraph">
-            Items you are likely to need soon will rise to the top of your list,
-            while items you probably won't need for a while will appear lower on
-            the list.
-          </p>
+
+        <section className="about-text__section about-section">
+          <img
+            src={phoneSample}
+            alt="Screenshot of adding items to your list"
+            className="about-section__image"
+          />
+          <div className="about-section__description">
+            <h2 className="about-section__heading">
+              Peasy predicts what you'll need to buy next
+            </h2>
+            <p className="about-section__paragraph">
+              Your list gets smarter with time! Peasy uses your buying history
+              to predict what you'll need to buy next and sort your list
+              automatically.
+            </p>
+            <p className="about-section__paragraph">
+              Items you are likely to need soon will rise to the top of your
+              list, while items you probably won't need for a while will appear
+              lower on the list.
+            </p>
+          </div>
         </section>
-        <section className="about-section">
-          <h2 className="about-section__header">Share your list</h2>
-          <p className="about-section__paragraph">
-            You can share the same list with everyone who shops for your
-            household. Simply provide your lists's three word token to your
-            friend or family member, and they can use it to join your list from
-            Peasy's home screen.
-          </p>
+
+        <section className="about-text__section about-section">
+          <img
+            src={phoneSample}
+            alt="Screenshot of adding items to your list"
+            className="about-section__image"
+          />
+          <div className="about-section__description">
+            <h2 className="about-section__heading">
+              Share your list with your household
+            </h2>
+            <p className="about-section__paragraph">
+              You can share the same list with everyone who shops for your
+              household. Simply provide your lists's three word token to your
+              friend or family member, and they can use it to join your list
+              from Peasy's home screen.
+            </p>
+          </div>
         </section>
-        <footer className=".page-view__footer">
-          {listId ? (
-            <NavLink to="/" className="about-button">
-              Back To List
-            </NavLink>
-          ) : (
-            <NavLink to="/" className="about-button">
-              Get Started
-            </NavLink>
-          )}
-          <p className=".page-view__footer about-footer">
-            {[
-              'Made with 💚 by Ander, Connie, Nick, & Sheila in partnership with ',
-              <a
-                className=".about-footer__attribution"
-                href="https://the-collab-lab.codes/"
-              >
-                The Collab Lab
-              </a>,
-            ]}
-          </p>
-        </footer>
+
+        {listId ? (
+          <NavLink
+            to="/"
+            className="button button_type_primary about-text__button"
+          >
+            &laquo; Back To List
+          </NavLink>
+        ) : (
+          <NavLink
+            to="/"
+            className="button button_type_primary about-text__button"
+          >
+            Get Started &raquo;
+          </NavLink>
+        )}
       </main>
+
+      <footer className="page-view__footer about-footer">
+        <p className="about-footer__attribution">
+          Made with{' '}
+          <span role="img" aria-label="heart">
+            💚
+          </span>{' '}
+          by Ander, Connie, Nick, & Sheila in partnership with&nbsp;
+          <a
+            className="link link_footer"
+            href="https://the-collab-lab.codes/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            The Collab Lab
+          </a>
+          .
+        </p>
+      </footer>
     </div>
   );
 };

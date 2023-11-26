c210cf083963bbe902ca603112b18e9268b75da0
DJ Taylor <dtaylor88@gmail.com>
2023-11-26 14:45:16 -0800

    ignore lint rule

--- src/components/AddItemForm/AddItemForm.js
+++ src/components/AddItemForm/AddItemForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { useState, useRef, useEffect } from 'react';
 
 import firebase from 'firebase/app';
--- src/components/ShoppingListItem/ShoppingListItem.js
+++ src/components/ShoppingListItem/ShoppingListItem.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 import { useState, useEffect, useRef } from 'react';
 import { DateTime } from 'luxon';
 
